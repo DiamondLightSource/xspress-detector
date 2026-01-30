ec2ffbeb2593e8b82d6364ef64a8dd2c4b1c76f8
Luis Segalla <Luis.Segalla@diamond.ac.uk>
2026-01-30 14:07:15 +0000

    Return constant for number of scalars

--- cpp/control/src/LibXspressSimulator.cpp
+++ cpp/control/src/LibXspressSimulator.cpp
@@ -606,7 +606,7 @@ int LibXspressSimulator::get_num_frames_read(int32_t *frames)
 
 int LibXspressSimulator::get_num_scalars(uint32_t *num_scalars)
 {
-  *num_scalars = 9;
+  *num_scalars = XSP3_SW_NUM_SCALERS;
   return XSP_STATUS_OK;
 }
 
